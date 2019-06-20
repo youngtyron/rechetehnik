d291395283387908445b04eefa9e041e5a2cbd0c
irinaoxymoron <irinaoxymoron@gmail.com>
2019-06-20 18:54:45 +0300

    final fix

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -1,12 +1,43 @@
-  $(document).ready(function(){
+$(document).ready(function(){
 
-    $(".menu-trigger").click(function () {
-      $(".ul-trigger").slideToggle();
-      $(".menu-trigger").toggleClass('menu_state_open');
-    });
-    $(window).resize(function(){
-      if ($(window).width() > 1024) {
-        $('.ul-trigger').removeAttr('style');
-      };
-    });
+  $(".menu-trigger").click(function () {
+    $(".ul-trigger").slideToggle();
+    $(".menu-trigger").toggleClass('menu_state_open');
   });
+  $(window).resize(function(){
+    if ($(window).width() > 1024) {
+      $('.ul-trigger').removeAttr('style');
+    };
+  });
+});
+
+$('.multiple-items').slick({
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  arrows : false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
