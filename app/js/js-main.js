8ce9dd904ed4c687fb356fcd0b11192788149849
irinaoxymoron <irinaoxymoron@gmail.com>
2019-08-12 16:53:41 +0300

    modal

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -21,31 +21,13 @@ $(document).ready(function(){
 });
 
 $('.multiple-items').slick({
-  slidesToShow: 6,
-  slidesToScroll: 6,
+  slidesToShow: 5,
+  slidesToScroll: 5,
   arrows : false,
   responsive: [
     {
       breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
       settings: "unslick"
     }
   ]
 });
-
-
