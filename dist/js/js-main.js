ad2bb1f649acce58fe0b472c65565ae69c318ca0
youngtyron <gro-jorge@mail.ru>
2020-03-27 10:02:20 +0300

    test plugged-in

--- dist/js/js-main.js
+++ dist/js/js-main.js
@@ -1,3 +1,5 @@
+var testMarks = 0;
+
 $(document).ready(function(){
 
   $(".menu-trigger").click(function () {
@@ -92,6 +94,7 @@ $('.show-modal2').on('click', function() {
 
 $('.close-modal2').on('click', function() {
  modal2.fadeOut();
+ resetTest();
 });
 }
 openLightBox2();
@@ -105,6 +108,7 @@ $('.show-modal3').on('click', function() {
 
 $('.close-modal3').on('click', function() {
  modal3.fadeOut();
+ resetTest();
 });
 }
 openLightBox3();
@@ -131,14 +135,30 @@ function nextStep(){
       $('.step2').removeClass('step_closed').addClass('step_opened');
       document.body.scrollTo(0,0);
       nextStep();
+      var testAnswers = $('.step2 .modal__radio input[type="checkbox"]');
+      $(testAnswers).on('click', testAnswerClick);
     } else if (next == 3) {
-      $('.step_opened').removeClass('step_opened').addClass('step_closed');
-      $('.step3').removeClass('step_closed').addClass('step_opened');
-      document.body.scrollTo(0,0);
-      nextStep();
+      if (testIsComplete()) {
+        $('.modal__notification').addClass('notification_closed');
+        if (testMarks<6) {
+          var result = $('.modal__final .red_mark').parent();
+        } else if (testMarks<18) {
+          var result = $('.modal__final .yellow_mark').parent();
+        } else {
+          var result = $('.modal__final .green_mark').parent();
+        }
+        $(result).removeClass('result_closed');
+        $('.step_opened').removeClass('step_opened').addClass('step_closed');
+        $('.step3').removeClass('step_closed').addClass('step_opened');
+        document.body.scrollTo(0,0);
+        nextStep();
+      } else {
+        pleaseCompleteTest();
+      }
     } else if (next == 'end') {
       $('.modal3').fadeOut();
       document.body.scrollTo(0,0);
+      resetTest();
     }
   });
 }
@@ -147,4 +167,55 @@ nextStep();
 function cancelSteps(){
   $('.step_opened').removeClass('step_opened').addClass('step_closed');
   $('.step1').removeClass('step_closed').addClass('step_opened');
+  resetTest();
+}
+
+function testAnswerClick(e) {
+  var mark = parseInt(e.target.value);
+  if (e.target.checked) {
+    testMarks += mark;
+    var otherVariants = $(e.target).parent().siblings();
+    $(otherVariants).each(function(){
+      var checkbox = $(this).children('input')[0];
+      if (checkbox.checked) {
+        var otherMark = parseInt(checkbox.value);
+        testMarks -= otherMark;
+        checkbox.checked = '';
+      }
+    });
+  } else {
+    testMarks -= mark;
+  }
+}
+
+function resetTest() {
+  $('.modal__final').each(function(){
+      $(this).addClass('result_closed');
+  });
+  var checkboxes = $('.modal__radio input');
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = '';
+  };
+  testMarks = 0;
+  $('.step2 .modal__radio input[type="checkbox"]').unbind('click', testAnswerClick); 
+  $('.modal__notification').addClass('notification_closed');
+}
+
+function testIsComplete() {
+    var checkboxes = $('.modal__radio input');
+    var answered = 0;
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        answered += 1;
+      }
+    }
+    var answersCount = $('.modal__radio__block').length;
+    if (answered<answersCount) {
+      return false;
+    }
+    return true;
+}
+
+function pleaseCompleteTest() {
+  $('.modal__notification').removeClass('notification_closed');
 }
